848622ff6915c592a33102941be7b82d23061add
atayarabbi <atayarabbi44544@gmail.com>
2021-05-19 19:36:11 +0600

    search

--- index.js
+++ index.js
@@ -15,53 +15,54 @@ const port = 5000;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const productsCollection = client.db("emaJohnStore").collection("products");
-  const ordersCollection = client.db("emaJohnStore").collection("orders");
-  
+    const productsCollection = client.db("emaJohnStore").collection("products");
+    const ordersCollection = client.db("emaJohnStore").collection("orders");
+
     app.post('/addProduct', (req, res) => {
         const products = req.body;
         console.log(products);
         productsCollection.insertOne(products)
-        .then(result => {
-            console.log(result.insertedCount);
-            res.send(result.insertedCount)
-        })
+            .then(result => {
+                console.log(result.insertedCount);
+                res.send(result.insertedCount)
+            })
     })
 
     app.get('/products', (req, res) => {
-        productsCollection.find({})
-        .toArray( (err, documents) => {
-            res.send(documents);
-        })
+        const search = req.query.search;
+        productsCollection.find({ name: { $regex: search } })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
 
     app.get('/product/:key', (req, res) => {
-        productsCollection.find({key: req.params.key})
-        .toArray( (err, documents) => {
-            res.send(documents[0]);
-        })
+        productsCollection.find({ key: req.params.key })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
     })
 
     app.post('/productsByKeys', (req, res) => {
         const productKeys = req.body;
-        productsCollection.find({key: { $in: productKeys} })
-        .toArray( (err, documents) => {
-            res.send(documents);
-        })
+        productsCollection.find({ key: { $in: productKeys } })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
 
     app.post('/addOrder', (req, res) => {
         const order = req.body;
         ordersCollection.insertOne(order)
-        .then(result => {
-            res.send(result.insertedCount > 0)
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
     })
 
 });
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  })
+})
